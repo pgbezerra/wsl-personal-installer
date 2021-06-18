456925790e3c4d8d85f833ee1c9cc936a2b43c09
Bezerra <paulobezerr@hotmail.com>
2021-06-18 20:33:36 -0300

    fix git install as user

--- configure_git.sh
+++ configure_git.sh
@@ -2,9 +2,9 @@
 
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
 
-su $USERNAME -c "git config --global user.name $GIT_NAME"
-su $USERNAME -c "git config --global user.email $GIT_EMAIL"
-su $USERNAME -c 'git config --global push.default current'
-su $USERNAME -c 'git config --global core.editor vim'
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+git config --global push.default current
+git config --global core.editor vim
 
 echo 'Git credendials and user configured.'
