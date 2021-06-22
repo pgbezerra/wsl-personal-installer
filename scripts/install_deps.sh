694022cd96f871588cc79bee69481e81d44dee26
Bezerra <paulobezerr@hotmail.com>
2021-06-21 21:06:10 -0300

    feat: install node 14 LTS

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apt-get install -y python-is-python3 python3-pip
+apt-get install -y python-is-python3 python3-pip build-essential libssl-dev
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh >nvm_installer.sh
+chmod +x nvm_installer.sh
+su $USERNAME -- nvm_installer.sh
+rm nvm_installer.sh
+
+su - $USERNAME -c '. $HOME/.nvm/nvm.sh && nvm install 14.17.1'
--- scripts/zsh/.zshrc
+++ scripts/zsh/.zshrc
@@ -20,3 +20,7 @@ antigen theme bira
 ​
 # Tell Antigen that you're done.
 antigen apply
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
